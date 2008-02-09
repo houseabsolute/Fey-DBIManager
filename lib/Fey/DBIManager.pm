83c0da7633c4fb300461eb60deebed716b43c012
autarch <devnull@localhost>
2008-02-09 18:40:57 +0000

    Changes for 0.03.
    Ignore generated files.

--- lib/Fey/DBIManager.pm
+++ lib/Fey/DBIManager.pm
@@ -3,7 +3,7 @@ package Fey::DBIManager;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Fey::Exceptions qw( object_state_error param_error );
 use Scalar::Util qw( blessed );
