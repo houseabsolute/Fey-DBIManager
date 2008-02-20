11ba5ed9434caf717a87033aa5e1581a293419cd
autarch <devnull@localhost>
2008-02-20 20:50:52 +0000

    Checking in changes prior to tagging of version 0.04.  Changelog diff is:
    Index: Changes
    ===================================================================
    --- Changes	(revision 35)
    +++ Changes	(working copy)
    @@ -1,4 +1,4 @@
    -0.04
    +0.04    2008-02-20
    
     - Add $source->allows_nested_transactions() attribute (primarily for
       the benefit off Fey::ORM).
    @@ -18,4 +18,3 @@
     0.01    2008-02-05
    
     - Initial release
    -

--- lib/Fey/DBIManager.pm
+++ lib/Fey/DBIManager.pm
@@ -3,7 +3,7 @@ package Fey::DBIManager;
 use strict;
 use warnings;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Fey::Exceptions qw( object_state_error param_error );
 use Scalar::Util qw( blessed );
