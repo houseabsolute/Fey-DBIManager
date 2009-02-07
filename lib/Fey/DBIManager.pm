1a4288406e94327f5001234b8fbf897c5d92e62b
Dave Rolsky <autarch@urth.org>
2009-02-06 18:26:59 -0600

    bump version, no Fey::Validate

--- lib/Fey/DBIManager.pm
+++ lib/Fey/DBIManager.pm
@@ -3,7 +3,7 @@ package Fey::DBIManager;
 use strict;
 use warnings;
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 use Fey::Exceptions qw( object_state_error param_error );
 use Scalar::Util qw( blessed );
